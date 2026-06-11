6b007b6729f40ce2ce843fdce0576a9cfafb5e20
ccgo14 <calebmuuo64@gmail.com>
2026-06-12 01:53:08 +0300

    feat:add property to user model

--- model/user.py
+++ model/user.py
@@ -1,4 +1,21 @@
-class user:
-    def __init__(self,name,email):
-        self.name = name
-        self.email = email
+"""Module for managing user data."""
+
+
+class USser:
+    """Represents a system user."""
+
+    def __init__(self, name, email):
+        """Initialize a new user with a name and email."""
+        self._name = name
+        self._email = email
+        self._project = []
+
+    @property
+    def name(self):
+        """Get the user's name."""
+        return self._name
+
+    @property
+    def email(self):
+        """Get the user's email."""
+        return self._email
