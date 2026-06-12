dae99bbba25ee0d02bbbad79a76d124049cb6292
ccgo14 <calebmuuo64@gmail.com>
2026-06-12 03:52:15 +0300

    fix: made sure class name wae pascale case

--- model/project.py
+++ model/project.py
@@ -1,12 +1,10 @@
-class project:
-    def __init__(self,title,description,due_date):
+class Project:
+    def __init__(self, title, description, due_date):
         self._title = title
         self._description = description
         self._due_date = due_date
-        self._tasks = []
+        self.tasks = []
+
     @property
     def title(self):
         return self._title
-
-    def add_task(self, task):
-        self._tasks.append(task)
